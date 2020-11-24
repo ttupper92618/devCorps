bff7c5cb0d3e56b663fa402bb09b422d94192eaa
ttupper92618 <thomastupper@Thomass-Mac-mini.local>
2020-11-24 15:54:28 -0600

    minor changes

--- App.js
+++ App.js
@@ -1,21 +1,20 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import styled from "styled-components";
+
+const Container = styled.View`
+  flex: 1;
+  background-color: #f0f3f5;
+  justify-content: center;
+  align-items: center;
+`;
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <Container>
+      <Text>My First App!</Text>
       <StatusBar style="auto" />
-    </View>
+    </Container>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
